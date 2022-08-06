0e09a10c70f92c4335ead2553c3ae34c2ebd1d8d
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-08-06 17:15:37 -0400

    inline things

--- sqlite3-bindings/src/Sqlite3/Bindings/Internal/Utils.hs
+++ sqlite3-bindings/src/Sqlite3/Bindings/Internal/Utils.hs
@@ -31,38 +31,47 @@ boolToCInt :: Bool -> CInt
 boolToCInt = \case
   False -> 0
   True -> 1
+{-# INLINE boolToCInt #-}
 
 cintToInt :: CInt -> Int
 cintToInt =
   fromIntegral
+{-# INLINE cintToInt #-}
 
 intToCInt :: Int -> CInt
 intToCInt =
   fromIntegral
+{-# INLINE intToCInt #-}
 
 cuintToWord :: CUInt -> Word
 cuintToWord =
   fromIntegral
+{-# INLINE cuintToWord #-}
 
 wordToCUInt :: Word -> CUInt
 wordToCUInt =
   fromIntegral
+{-# INLINE wordToCUInt #-}
 
 cdoubleToDouble :: CDouble -> Double
 cdoubleToDouble =
   realToFrac
+{-# INLINE cdoubleToDouble #-}
 
 doubleToCDouble :: Double -> CDouble
 doubleToCDouble =
   realToFrac
+{-# INLINE doubleToCDouble #-}
 
 cstringToText :: CString -> IO Text
 cstringToText string =
   Text.fromPtr0 (castPtr string)
+{-# INLINE cstringToText #-}
 
 cstringLenToText :: Ptr CChar -> Text.I8 -> IO Text
 cstringLenToText c_string len =
   Text.fromPtr (castPtr c_string) len
+{-# INLINE cstringLenToText #-}
 
 textToCString :: Text -> (CString -> IO a) -> IO a
 textToCString text@(Text _ _ len) action =
@@ -70,12 +79,14 @@ textToCString text@(Text _ _ len) action =
     Text.unsafeCopyToPtr text ptr
     Storable.pokeByteOff ptr len (0 :: Word8)
     action (castPtr ptr)
+{-# INLINE textToCString #-}
 
 textToCStringLen :: forall a. Text -> (Ptr CChar -> Int -> IO a) -> IO a
 textToCStringLen text@(Text _ _ len) action =
   allocaBytes len \ptr -> do
     Text.unsafeCopyToPtr text ptr
     action (castPtr ptr) len
+{-# INLINE textToCStringLen #-}
 
 carrayToArray :: forall a b. Storable a => (a -> IO b) -> CInt -> Ptr a -> IO (Array Int b)
 carrayToArray convert (cintToInt -> n) p0 = do
@@ -89,3 +100,4 @@ carrayToArray convert (cintToInt -> n) p0 = do
           loop (i + 1) (p0 `plusPtr` Storable.sizeOf (undefined :: Ptr ()))
   loop 0 p0
   Array.IO.freeze arr
+{-# INLINE carrayToArray #-}
